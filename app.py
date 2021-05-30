d08ae21d01f5f64c001eff2b1ffde272a70b7dfc
Ferdosull <ferdia.osullivan@gmail.com>
2021-05-30 07:44:17 +0000

    Add profile template

--- app.py
+++ app.py
@@ -29,6 +29,10 @@ def get_recipes():
 def login():
     return render_template("login.html")
 
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
