20e80b99ee272586aa4a34b3575aac9b767930c3
Ferdosull <ferdia.osullivan@gmail.com>
2021-06-06 15:27:36 +0000

    Add JS functionality to hide show buttons & inputs on recipe form

--- static/js/script.js
+++ static/js/script.js
@@ -5,6 +5,15 @@ $(document).ready(function(){
     $("#select1").formSelect();
     $("#select2").formSelect();
     $("#select3").formSelect();
+    // $("#step2div").hide();
+    // $("#step3div").hide();
+    // $("#step4div").hide();
+    // $("#step5div").hide();
+    // $("#step6div").hide();
+    // $("#step7div").hide();
+    // $("#step8div").hide();
+    // $("#step9div").hide();
+    // $("#step10div").hide();
 });
 
 $("#toTop").click(function () {
@@ -34,3 +43,13 @@ function validatePassword(){
 confirm_password.onkeyup = validatePassword;
 
 // Password Validation //
+
+function initStep(a, b, c) { // using button clicks and respective key values to update the function from the object //
+    var inputField = document.getElementById(a); // getting the header by id //
+    inputField.classList.remove('hide-at-pg-load'); // removing the show yourself class //
+    var hideButton = document.getElementById(b); // getting the header by id //
+    hideButton.classList.add('hide-at-pg-load'); // removing the show yourself class //
+    var showButton = document.getElementById(c); // getting the header by id //
+    showButton.classList.remove('hide-at-pg-load'); // removing the show yourself class //
+    // transition.classList.add('hide'); // hiding the header element with opacity transition //
+}
